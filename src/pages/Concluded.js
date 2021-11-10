15f171fbc3459aa5806df6e998d304e9b4645509
Saulo Santana <saulocostasan@gmail.com>
2021-11-10 04:26:32 -0300

    adding concluded page

---
+++ src/pages/Concluded.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import * as St from "./styled";
+
+import illustration from "../assets/images/illustration.png";
+
+export class Concluded extends Component {
+  render() {
+    return (
+      <>
+        <St.Sucess>
+          <img src={illustration} />
+          <St.TitleSucess>Sua reserva foi feita com sucesso!</St.TitleSucess>
+          <St.TextSucess>
+            Você receberá um e-mail com a confirmação e mais detalhes sobre sua
+            reserva. Estamos ansiosos para recebê-lo!
+          </St.TextSucess>
+        </St.Sucess>
+      </>
+    );
+  }
+}
