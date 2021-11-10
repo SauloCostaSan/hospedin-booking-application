bdb6a95c6f306dbe009f66d75245ef52f85e924d
Saulo Santana <saulocostasan@gmail.com>
2021-11-10 04:48:01 -0300

    adding route folder

---
+++ src/routes/Routers.js
@@ -0,0 +1,20 @@
+// import Accommodation from '../pages/Accommodation';
+// import Acquisition from '../pages/Acquisition';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Concluded } from "../pages/Concluded";
+import { Payment } from "../pages/Payment";
+import { Booking } from "../pages/Booking";
+
+export function Routers() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Booking />}></Route>
+        <Route path="/payment" element={<Payment />}>
+        </Route>
+        <Route exact path="/concluded" element={<Concluded />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
