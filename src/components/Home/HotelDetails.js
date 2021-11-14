6f03e1f69fefcda91793699be3cec20d02c7d575
Saulo Santana <saulocostasan@gmail.com>
2021-11-13 23:37:07 -0300

    updating the hotel details page

--- src/components/Home/HotelDetails.js
+++ src/components/Home/HotelDetails.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React , { useState } from "react";
 import { CommodityDetails } from "./ComodityDetails";
 
 import * as Gl from "../../generalStyled/variables";
 import * as St from "./styled";
 
 import mapIcon from "../../assets/images/svg/map-icon.svg";
+import arrowDown from "../../assets/images/svg/arrow-down-icon.svg";
+import arrowUp from "../../assets/images/svg/arrow-up-icon.svg";
 
 export const HotelDetails = () => {
-    return (
-      <>
+  const [showDetails, setShowDetails] = useState(false);
+  const [changeArrow, setChangeArrow] = useState(false);
+  const [changeText, setChangeText] = useState(false);
+
+  const expandDetails = () => {
+    setShowDetails((prevState) => !prevState);
+    setChangeArrow((prevState) => !prevState);
+    setChangeText((prevState) => !prevState);
+  };
+
+  return (
+    <>
       <Gl.Container>
         <St.Inn>
           <St.InnInformation>
             <Gl.H1>{"Pousada Na Tribu's"}</Gl.H1>
-            <St.InnDescription>
+            <St.InnDescription id="description">
               Próximo aos setores de entretenimento e empresarial, o Plaza
               Bittar Hotel fica a 1 quarteirão do Shopping Brasília e a 15
               minutos de carro do Aeroporto Internacional de Brasília. Os
-              quartos dispõem de ar-condicionado, e o Wi-Fi...
+              quartos dispõem de ar-condicionado, e o Wi-Fi { " " }
+              {showDetails ? (
+                <St.DetailsText>
+                  de ótima qualidade edisponível em todas as dependências da
+                  pousada, o melhor lugar para sua hospedagem na região.
+                </St.DetailsText>
+              ) : (
+                <St.DetailsText> ...</St.DetailsText>
+              )}
             </St.InnDescription>
+              <St.ExpandBtn onClick= {expandDetails}>
+                <St.Icon src={changeArrow ? arrowUp : arrowDown} />
+                <St.ExpandText>{changeText ? `Ocultar descrição do hotel ` : `Expandir descrição do hotel`}</St.ExpandText>
+              </St.ExpandBtn>
 
             <St.Commodity>
               <Gl.H2>Comodidades do hotel</Gl.H2>
@@ -54,8 +78,7 @@ export const HotelDetails = () => {
             </St.CheckInfo>
           </St.InnAddress>
         </St.Inn>
-        </Gl.Container>
-      </>
-    );
-  }
-
+      </Gl.Container>
+    </>
+  );
+};
