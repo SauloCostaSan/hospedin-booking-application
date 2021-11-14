584f15fc72449d04f29c462799992c95819bdb9f
Saulo Santana <saulocostasan@gmail.com>
2021-11-13 23:46:15 -0300

    updating routes

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -3,18 +3,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Concluded } from "../pages/Concluded";
-import { Payment } from "../pages/Payment";
 import { Booking } from "../pages/Booking";
+import { Payment } from "../pages/Payment";
 
 export function Routers() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Booking />}></Route>
-        <Route exact path="/payment:id" element={<Payment />}>
-        </Route>
-        <Route exact path="/concluded" element={<Concluded />}></Route>
+        <Route exact path="/" element={<Booking />} />
+        <Route exact path="/payment/:nameRoom/:price/:dateIn/:dateOut" element={<Payment />} />
+        <Route exact path="/concluded" element={<Concluded />} />
       </Routes>
     </BrowserRouter>
   );
--- src/routes/navigate.js
+++ src/routes/navigate.js
@@ -1,9 +1,9 @@
 export const homeBooking = (navigate) => {
   navigate.push("/")
 };
-export const routePayment = (navigate, id) => {
-  navigate.push(`payment/${id}`)
+export const routePayment = (navigate) => {
+  navigate.push(`/payment/:name/:price/:checkIn/:checkOut`)
 };
-export const concludedBooking = (navigate, id) => {
+export const concludedBooking = (navigate) => {
   navigate.push(`/concluded`)
 };
