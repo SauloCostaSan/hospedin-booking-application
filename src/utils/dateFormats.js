bbd4d2f6b19cec27b22aa458d4c896d2b7834a0b
Saulo Santana <saulocostasan@gmail.com>
2021-11-13 23:47:49 -0300

    inserting file to format dates

---
+++ src/utils/dateFormats.js
@@ -0,0 +1,25 @@
+export const daysLong = [
+  "domingo",
+  "segunda-feira",
+  "terça-feira",
+  "quarta-feira",
+  "quinta-feira",
+  "sexta-feira",
+  "sábado"
+];
+
+export const monthsLong = [
+  "janeiro",
+  "fevereiro",
+  "março",
+  "abril",
+  "maio",
+  "junho",
+  "julho",
+  "agosto",
+  "setembro",
+  "outubro",
+  "novembro",
+  "dezembro"
+];
+
