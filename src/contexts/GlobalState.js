019c4c2dd633e391ae32f3772d5e1341be5e4c40
Saulo Santana <saulocostasan@gmail.com>
2021-11-14 00:34:42 -0300

    removing the priceRoom const that was not being used

--- src/contexts/GlobalState.js
+++ src/contexts/GlobalState.js
@@ -4,7 +4,6 @@ import { GlobalContext } from "./GlobalContext";
 export const GlobalState = (props) => {
   const [checkIn, setCheckIn] = useState(null);
   const [checkOut, setCheckOut] = useState(null);
-  const [priceRoom, setPriceRoom] = useState('');
 
   const states = { checkIn, checkOut};
   const setters = { setCheckIn, setCheckOut};
