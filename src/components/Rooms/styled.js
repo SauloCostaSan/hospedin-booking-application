cf0999ae9be7af3c608c8d980f9341d9ac12e2fd
Saulo Santana <saulocostasan@gmail.com>
2021-11-13 23:41:56 -0300

    updating room component styles

--- src/components/Rooms/styled.js
+++ src/components/Rooms/styled.js
@@ -1,16 +1,31 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Button } from "../User/Button";
 
 import styled from "styled-components";
-import { variable, H1, H2, H3, SmallText, Container } from "../../generalStyled/variables";
+import {
+  variable,
+  H3,
+  Container,
+} from "../../generalStyled/variables";
 
 import arrowDown from "../../assets/images/svg/arrow-down-icon.svg";
+import calendar from "../../assets/images/svg/calendar-icon.svg";
 
+//styles from the amenities list
 export const AmenitiesList = styled.div`
   display: flex;
   align-items: center;
 `;
+export const Commodity = styled.div`
+  grid-row: 2;
+`;
+export const CommodityIcon = styled.img`
+  width: 16px;
+  height: 16px;
+  margin-right: 8px;
+`;
+
+//initial mode styles before choosing dates
 export const InitialSection = styled.div`
   background-color: ${variable.colors.white};
   border: 1px solid ${variable.colors.borderColor};
@@ -27,73 +42,8 @@ export const SelectDates = styled.h1`
   font-size: ${variable.fontSize[3]};
   color: ${variable.colors.red};
 `;
-export const HotelDates = styled.section`
-  display: flex;
-  flex-direction: column;
-  background-color: ${variable.colors.lightBlue};
-  width: 100%;
-  padding-bottom: 32px;
-`;
-export const ContainerReserve = styled(Container)`
-  flex-direction: column;
-`;
-export const ReserveDate = styled.div`
-  /* width: 50%; */
-  padding-top: 40px;
-`;
-export const Division = styled.div`
-  margin: 40px 0;
-  height: 1px;
-  background-color: ${variable.colors.borderColor};
-`;
-export const Dates = styled.div`
-  background-color: ${variable.colors.white};
-  border: 1px solid ${variable.colors.borderColor};
-  border-radius: ${variable.radius[0]};
-  width: fit-content;
-  display: flex;
-  align-items: center;
-  margin-top: 8px;
-`;
-export const DateInput = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-export const Arrow = styled.img`
-  margin-right: 16px;
-`;
-export const Picker = styled(DatePicker)`
-  border: none;
-  padding: 16px;
-  margin-right: 16px;
-  color: ${variable.colors.blueTexts};
-  font-family: ${variable.fonts.roboto};
-  font-weight: ${variable.fontWeight[0]};
-  font-size: ${variable.fontSize[2]};
-  line-height: 19px;
-  background: url(${arrowDown});
-  background-repeat: no-repeat;
-  background-position: right;
-`;
-export const DivisionDate = styled.div`
-  width: 2px;
-  height: 32px;
-  background-color: ${variable.colors.borderColor};
-`;
-export const CalendarBox = styled.div`
-  padding: 16px;
-  border-left: 1px solid ${variable.colors.borderColor};
-`;
-export const Calendar = styled.img`
-  width: 24px;
-  height: 24px;
-`;
-export const SectionRooms = styled(Container)`
-  max-width: 960px;
-  width: 100%;
-  flex-direction: column;
-`;
+
+//available room card styles
 export const RoomCard = styled(Container)`
   max-width: 960px;
   width: 100%;
@@ -102,24 +52,6 @@ export const Room = styled.div`
   display: grid;
   grid-template: repeat(3, auto) / 4fr 1fr 1fr 2fr;
   width: 100%;
-`;
-export const TitleType = styled(H3)`
-  grid-column-start: 1;
-  grid-column-end: 1;
-  white-space: nowrap;
-`;
-export const TitlePrice = styled(H3)`
-  grid-column-start: 2;
-  grid-column-end: 2;
-  white-space: nowrap;
-  text-align: right;
-  margin-right: 24px;
-`;
-export const TitleCapacity = styled(H3)`
-  grid-column-start: 3;
-  grid-column-end: 3;
-  white-space: nowrap;
-  text-align: center;
 `;
 export const TypeRoom = styled.div`
   background-color: ${variable.colors.white};
@@ -143,7 +75,6 @@ export const TextRoom = styled.p`
   font-size: ${variable.fontSize[3]};
   line-height: ${variable.lineHeight[0]};
   color: ${variable.colors.blueTitles};
-  /* max-width: 227px; */
 `;
 export const PriceRoom = styled.div`
   background-color: ${variable.colors.white};
@@ -178,7 +109,6 @@ export const ReserveBtn = styled.div`
   border: 1px solid ${variable.colors.borderColor};
   border-radius: 0 ${variable.radius[0]} 0 0;
   border-left: none;
-  /* grid-row: 2 / 2; */
   grid-column: 4 / 4;
   display: flex;
   align-items: center;
@@ -197,22 +127,15 @@ export const CardFooter = styled.div`
   flex-direction: column;
   justify-content: flex-start;
 `;
-export const MoreInfo = styled.div`
-  padding: 8px 16px;
-`;
 export const MoreInfoExpand = styled.div`
   padding-left: 16px;
   max-width: 960px;
   width: 100%;
   box-sizing: border-box;
-`;
-
-export const IconInfo = styled.img`
-  padding-right: 15px;
+  border-bottom: 1px solid ${variable.colors.borderColor};
 `;
 export const Info = styled.div`
   grid-column: 1 / span 4;
-  /* padding-top: 13px; */
   padding: 13px 0 16px 0;
   &:nth-child(2) {
     padding: 0;
@@ -221,30 +144,153 @@ export const Info = styled.div`
 export const H3Info = styled(H3)`
   padding-bottom: 6px;
 `;
+export const RoomCommodity = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 25px;
+  padding: 13px 0 16px 0;
+  justify-content: space-between;
+`;
 export const H3Amenities = styled(H3)`
   padding-bottom: 6px;
 `;
-export const RoomCommodity = styled.div`
-  grid-column: 1 / span 4;
-  padding: 13px 0 16px 0;
-  /* flex-direction: column; */
-`;
-export const Commodity = styled.div`
-  /* grid-column: 1 / span 4; */
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  align-items: flex-start;
-`;
-export const CommodityIcon = styled.img`
-  width: 16px;
-  height: 16px;
-  margin-right: 8px;
-`;
-export const CommodityText = styled.span``;
 export const Expand = styled.button`
   box-sizing: border-box;
-  padding: 8px;
+  padding: 12px;
   width: 100%;
-  border-top: 1px solid ${variable.colors.borderColor};
+  border: none;
+  background-color: ${variable.colors.white};
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
 `;
+export const IconInfo = styled.img`
+  padding-right: 15px;
+`;
+
+//styles of the date selection section
+export const HotelDates = styled.section`
+  display: flex;
+  flex-direction: column;
+  background-color: ${variable.colors.lightBlue};
+  width: 100%;
+  padding-bottom: 32px;
+`;
+export const ContainerReserve = styled(Container)`
+  flex-direction: column;
+`;
+export const ReserveDate = styled.div`
+  padding-top: 40px;
+`;
+export const Dates = styled.div`
+  background-color: ${variable.colors.white};
+  border: 1px solid ${variable.colors.borderColor};
+  border-radius: ${variable.radius[0]};
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  margin-top: 8px;
+`;
+export const DateInput = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const Picker = styled(DatePicker)`
+  border: none;
+  padding: 16px;
+  margin-right: 16px;
+  color: ${variable.colors.blueTexts};
+  font-family: ${variable.fonts.roboto};
+  font-weight: ${variable.fontWeight[0]};
+  font-size: ${variable.fontSize[2]};
+  line-height: 19px;
+  background: url(${arrowDown});
+  background-repeat: no-repeat;
+  background-position: right;
+`;
+export const DivisionDate = styled.div`
+  width: 2px;
+  height: 32px;
+  background-color: ${variable.colors.borderColor};
+`;
+export const PickerCalendar = styled(DatePicker)`
+  padding: 18px 16px;
+  width: 32px;
+  background-image: url(${calendar});
+  background-repeat: no-repeat;
+  background-position: center;
+  border: none;
+  border-left: 1px solid ${variable.colors.borderColor};
+  color: rgba(255, 255, 255, 0);
+`;
+export const Division = styled.div`
+  margin: 40px 0;
+  height: 1px;
+  background-color: ${variable.colors.borderColor};
+`;
+export const SectionRooms = styled(Container)`
+  max-width: 960px;
+  width: 100%;
+  flex-direction: column;
+`;
+export const TitleType = styled(H3)`
+  grid-column-start: 1;
+  grid-column-end: 1;
+  white-space: nowrap;
+`;
+export const TitlePrice = styled(H3)`
+  grid-column-start: 2;
+  grid-column-end: 2;
+  white-space: nowrap;
+  text-align: right;
+  margin-right: 24px;
+`;
+export const TitleCapacity = styled(H3)`
+  grid-column-start: 3;
+  grid-column-end: 3;
+  white-space: nowrap;
+  text-align: center;
+`;
+
+
+
+
+
+
+// export const Arrow = styled.img`
+//   margin-right: 16px;
+// `;
+
+
+
+
+// export const CalendarBox = styled.div`
+
+// `;
+// export const Calendar = styled.img`
+//   width: 24px;
+//   height: 24px;
+// `;
+
+
+
+
+
+
+
+
+
+
+// export const MoreInfo = styled.div`
+//   padding: 8px 16px;
+// `;
+
+
+
+
+
+
+// export const CommodityText = styled.span``;
+
