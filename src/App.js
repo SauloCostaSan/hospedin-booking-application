ecf535c4351f564b174f2910be1c7c975375f9ad
Saulo Santana <saulocostasan@gmail.com>
2021-11-13 23:49:22 -0300

    updating App and package.json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routers } from "./routes/Routers";
 import { GlobalState } from "./contexts/GlobalState";
 import { GlobalStyle } from "./generalStyled/GlobalStyles";
 
+
 export default function App() {
   return (
     <GlobalState>
