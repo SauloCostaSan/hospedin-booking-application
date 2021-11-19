58e6055afec5b81a9cbdf1a508a9abc6d95eb0ce
Saulo Santana <saulocostasan@gmail.com>
2021-11-19 15:41:09 -0300

    correcting daily calculation

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -15,7 +15,8 @@ export const Payment = () => {
   let checkOut = params.dateOut;
   let checkIn = params.dateIn;
 
-  const days = new Date(checkOut).getDate() - new Date(checkIn).getDate();
+  const days = Math.abs(new Date(checkOut) - new Date(checkIn));
+  const diffDays = Math.ceil( days / (1000 * 60 * 60 * 24));
 
   function dateFormatted(date) {
     return (
@@ -118,7 +119,7 @@ export const Payment = () => {
                     Duração total da hospedagem:{" "}
                   </St.SubtitleResume>
                   <St.TextResume>
-                    {days > 1 ? `${days} diárias` : `${days} diária`}
+                    {diffDays > 1 ? `${diffDays} diárias` : `${diffDays} diária`}
                   </St.TextResume>
                 </St.Details>
               </St.ReserveResume>
